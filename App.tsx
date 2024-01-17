5076ae2fc527057825afd782137c0b2ff362c102
Мөнхтэнгис Одбаяр <HP235999@G302-CC0097.local>
2024-01-17 19:45:57 +0800

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { Dog } from "./screens/Dog";
+import { Cat } from "./screens/Cat";
+import { HomeIcon } from "./icons/DogIcon";
+import { InfoIcon } from "./icons/CatIcon";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: "brown",
+          headerShown: false,
+          tabBarItemStyle: { marginBottom: -30 },
+        }}
+      >
+        <Tab.Screen
+          name="dog"
+          component={Dog}
+          options={{
+            tabBarIcon: ({ color }) => <HomeIcon color={color} />,
+            tabBarShowLabel: false,
+          }}
+        />
+        <Tab.Screen
+          name="Cat"
+          component={Cat}
+          options={{
+            tabBarIcon: ({ color }) => <InfoIcon color={color} />,
+            tabBarShowLabel: false,
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
+const HomeTab = createBottomTabNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const HomeTabScreens = () => {
+  return (
+    <HomeTab.Navigator screenOptions={{ headerShown: false }}>
+      <HomeTab.Screen name="HomeTab" component={Dog} />
+      <HomeTab.Screen name="AboutTab" component={Cat} />
+    </HomeTab.Navigator>
+  );
+};
+
+// const Stack = createStackNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator screenOptions={{ headerShown: false }}>
+//         <Stack.Screen name="FirstScreen" component={HomeTabScreens} />
+//         <Stack.Screen name="Home1" component={Home1} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
---
+++ screens/Cat.tsx
@@ -0,0 +1,31 @@
+import { FlatList, Text, View } from "react-native";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export const Cat = () => {
+  const [data, setData] = useState([]);
+  const instance = axios.create({
+    baseURL: "https://api.thecatapi.com/v1/breeds?limit=20",
+  });
+  const getItems = async () => {
+    try {
+      const res = await instance.get("/");
+      setData(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getItems();
+  }, []);
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <FlatList
+        data={data}
+        renderItem={(info: { index: number; item: any }) => (
+          <Text style={{padding:20,fontSize:20,fontStyle:"italic",fontWeight:"bold",borderStyle:"dashed",borderWidth:2,width:300}}>{info.item.name}</Text>
+        )}
+      ></FlatList>
+    </View>
+  );
+};
